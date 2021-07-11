33de80f0711d05ecee922532b124630f14f936bc
Srijha <srijha.1999@gmail.com>
2021-07-11 23:26:03 +0300

    added port

--- code/app.py
+++ code/app.py
@@ -300,6 +300,7 @@ def viz():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="127.0.0.1", debug=True, use_reloader=True)
    
     
