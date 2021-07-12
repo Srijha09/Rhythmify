b2f1f2b323be191d173131434b68a7ce161e2b8c
Srijha <srijha.1999@gmail.com>
2021-07-12 11:20:06 +0300

    Update app.py

--- code/app.py
+++ code/app.py
@@ -299,8 +299,13 @@ def viz():
 
 
 
+production = config.PRODUCTION
+
 if __name__ == '__main__':
-    #port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True)
+    if production:
+        app.run(debug=True)
+    else:
+        app.run(host='127.0.0.1', port=8002, debug=True)
+        # app.run())
    
     
