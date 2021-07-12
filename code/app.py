cfa183933f449a96a418711cb7f0304de6f9e368
Srijha <srijha.1999@gmail.com>
2021-07-12 10:42:52 +0300

    Update app.py

--- code/app.py
+++ code/app.py
@@ -300,7 +300,7 @@ def viz():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host="127.0.0.1", debug=True, use_reloader=True)
+    #port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True)
    
     
